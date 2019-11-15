96be4a7b40af9e8b17fb045335a760e260dd7a71
Eduard Kutsenko <eduardvad@gmail.com>
2019-11-15 14:10:07 -0600

    Initial commit of 2 projects

---
+++ ext-systems/city-register/src/main/resources/register.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+
+1. Rayon
+2. Ulitsa
+3. Adress - rayon, ulitsa, kpp, kv
+4. Persona - FIO,  data rozd, passport(seria, nomer. data vidachi), Svidet o rozdenii(nomer / data vid);
+5. Istoriya persona / adress - Persona, adress i diapazon dat vid registrazii;
+6.
---
+++ ext-systems/city-register/src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test(){
+        System.out.println("Running....");
+        Assert.assertTrue(true);
+    }
+}
