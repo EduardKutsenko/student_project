eda5191a867ceaeb51a9904d962c1598aa29bf7d
Eduard Kutsenko <eduardvad@gmail.com>
2019-11-21 11:56:07 -0600

    next commit

--- ext-systems/city-register/src/main/resources/register.sql
+++ ext-systems/city-register/src/main/resources/register.sql
@@ -35,7 +35,8 @@ CREATE TABLE cr_address(
 );
 
 INSERT INTO cr_address(district_code, street_code, building, extention, apartment)
-VALUES(1, 1, '3', 'EX', '4657A');
+VALUES(1, 1, '3', 'EX', '4657A'),
+(1, 1, '3', null, '4');
 
 
 CREATE TABLE cr_person(
@@ -55,16 +56,28 @@ CREATE TABLE cr_person(
 INSERT INTO cr_person(sur_name, given_name, patronimic_name, date_of_birth, passport_seria, passport_number,
 passport_date, certificate_number, certificate_date)
 VALUES('Semenov', 'Pavel', 'Trifonovich', '1995-03-09', '123', '535235234', '2010-09-09', null, null),
-('Semenova', 'Polina', 'Borisobna', '1996-03-09', '126', 'AA8977', '2013-01-09', null, null);
+('Semenova', 'Polina', 'Borisobna', '1996-03-09', '126', 'AA8977', '2013-01-09', null, null),
 ('Semenova', 'Irina', 'Pavlovna', '2015-06-09', null, null, null, 'RA3434', '2015-06-19'),
 ('Semenov', 'Igor', 'Pavlovich', '2019-08-05', null, null, null, 'RB999', '2019-08-15');
 
 
 CREATE TABLE cr_address_person(
-    person_address_id integer not null,
+    person_address_id SERIAL,
+    address_id integer not null,
     person_id integer not null,
     start_date date not null,
-    end_date date not null,
+    end_date date,
+    temporal boolean DEFAULT false,
     PRIMARY KEY(person_address_id),
+    FOREIGN KEY(address_id) REFERENCES cr_address(address_id) ON DELETE RESTRICT,
     FOREIGN KEY(person_id) REFERENCES cr_person(person_id) ON DELETE RESTRICT
 );
+
+INSERT INTO cr_address_person(address_id, person_id, start_date, end_date, temporal)
+VALUES
+      (1, 1, '2014-10-12', null, false),
+      (2, 2, '2014-10-12', null, true),
+      (1, 3, '2016-02-05', null, false),
+      (1, 4, '2017-07-15', null, false);
+
+
